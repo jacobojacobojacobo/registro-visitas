6b59d4717d68cda6d5a6c75543db0bd0b9e095d8
jacobojacobojacobo <cualquier_correo@example.com>
2025-11-18 11:00:22 -0500

    Actualizar app.py para manejar desconexión SSL

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from dotenv import load_dotenv
 import requests
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 # Cargar variables del .env
 load_dotenv()
@@ -13,9 +15,13 @@ app = Flask(__name__)
 # Verificar que DATABASE_URL sí se cargó
 print("USANDO DB:", os.getenv("DATABASE_URL"))
 
-# Configurar DB Neon
+# Configurar DB Neon con pool_pre_ping y pool_recycle
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    "pool_pre_ping": True,     # verifica que la conexión siga viva antes de usarla
+    "pool_recycle": 1800       # reinicia conexiones cada 30 minutos
+}
 
 db = SQLAlchemy(app)
 
